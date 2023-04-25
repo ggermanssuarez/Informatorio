e3022c9c6a2187b5818b4a9f686f269bdbdb9265
carolinasott <carolinnasottelo@gmail.com>
2023-04-25 16:58:46 -0300

    primer commit

---
+++ ejercicios-clase2.py
@@ -0,0 +1,34 @@
+## ejercicio 1 
+##nombre = input("por favor ingrese su nombre: ")
+##print(nombre)
+#---------------------------------------
+
+## Ejercicio 2
+##nombre = input("por favor ingrese su nombre: ")
+## print("Bienvenido" , nombre)
+##---------------------------
+
+## ejercicio 3 edad
+##edad = input("escribe tu edad: ")
+##print(edad)
+##----------------------------
+
+##ejercicio 4 suma
+##numero= 1+1
+##print(numero)
+##------------------------
+
+##ejercicio 5 cociente y resto
+##dividendo = int(input("por favor ingrese el dividendo: "))
+##divisor = int(input("por favor ingrese el divisor: "))
+##print("el cociente es ", (dividendo/divisor), "el resto es " , (dividendo%divisor))
+##-----------------
+## ejrcicio 6 radio y circulo
+##radio = int(input("por favor ingrese el radio: "))
+##pi = 3.1416
+##print("el area es ", (pi*radio**2))
+##---------------------------------------
+ ##ejercicio 7 triangulo
+base = int(input("ingrese la base "))
+altura= int(input("ingrese la altura "))
+print("el area es " , (base* altura)/2)
